94599e349b683fb2894c625cea45f78d4a041ad2
Adarsh Srivastava <imadarshsri@gmail.com>
2021-08-31 07:32:06 +0530

    Refactors code

--- lib/modules/video_conf/screens/classroom_channel.dart
+++ lib/modules/video_conf/screens/classroom_channel.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'classroom_video_call.dart';
 import 'package:universal_html/html.dart' as html;
@@ -17,7 +16,6 @@ class Channel extends StatefulWidget {
 class _ChannelState extends State<Channel> {
   late TextEditingController _channelController;
   late bool _validateError;
-  late ClientRole? _role;
 
   @override
   void dispose() {
@@ -28,7 +26,6 @@ class _ChannelState extends State<Channel> {
   @override
   void initState() {
     super.initState();
-    _role = ClientRole.Broadcaster;
     _channelController = TextEditingController(text: 'ch1');
     _validateError = false;
   }
@@ -60,10 +57,8 @@ class _ChannelState extends State<Channel> {
           );
           return;
         }
-        // final stream = await html.window.navigator.getUserMedia(video: true);
-
       }
-      await Navigator.push(
+      Navigator.push(
         context,
         MaterialPageRoute(
           builder: (_) => VideoCall(),
@@ -95,38 +90,10 @@ class _ChannelState extends State<Channel> {
                         border: UnderlineInputBorder(
                           borderSide: BorderSide(width: 1),
                         ),
-                        hintText: 'Channel name',
+                        hintText: 'Classroom Id',
                       ),
                     ),
                   ),
-                ],
-              ),
-              Column(
-                children: [
-                  ListTile(
-                    title: Text(ClientRole.Broadcaster.toString()),
-                    leading: Radio(
-                      value: ClientRole.Broadcaster,
-                      groupValue: _role,
-                      onChanged: (ClientRole? value) {
-                        setState(() {
-                          _role = value;
-                        });
-                      },
-                    ),
-                  ),
-                  ListTile(
-                    title: Text(ClientRole.Audience.toString()),
-                    leading: Radio(
-                      value: ClientRole.Audience,
-                      groupValue: _role,
-                      onChanged: (ClientRole? value) {
-                        setState(() {
-                          _role = value;
-                        });
-                      },
-                    ),
-                  )
                 ],
               ),
               Padding(
@@ -137,10 +104,6 @@ class _ChannelState extends State<Channel> {
                       child: ElevatedButton(
                         onPressed: onJoin,
                         child: Text('Join'),
-                        style: ButtonStyle(
-                            // backgroundColor: Colors.blueAccent,
-                            // foregroundColor: Colors.white,
-                            ),
                       ),
                     )
                   ],
